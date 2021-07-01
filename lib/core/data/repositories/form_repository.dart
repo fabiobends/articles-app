3b10bb8a5e322c4da95c625a6cc4a2f57ef6292b
Fábio Mendes <fabiobends@gmail.com>
2021-07-01 16:58:54 -0300

    Add refactored structure

--- lib/core/data/repositories/form_repository.dart
+++ lib/core/data/repositories/form_repository.dart
@@ -1,7 +1,7 @@
 class FormRepository {
   String checkEmail(String email) {
     final emailRegex = RegExp(
-        r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$');
+        r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)+$');
 
     var isValid = emailRegex.hasMatch(email);
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'core/bloc/email/email_bloc.dart';
-import 'core/bloc/form/form_bloc.dart';
-import 'core/bloc/password/password_bloc.dart';
-import 'core/data/repositories/form_repository.dart';
-import 'pages/login_page.dart';
+import 'core/bloc/auth_form/auth_form_bloc.dart';
+// import 'core/data/repositories/form_repository.dart';
+import 'core/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,25 +12,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var _formRepository = FormRepository();
+    // var _formRepository = FormRepository();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Articles App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MultiBlocProvider(
-        providers: [
-          BlocProvider<EmailBloc>(
-            create: (BuildContext context) => EmailBloc(_formRepository),
-          ),
-          BlocProvider<PasswordBloc>(
-            create: (BuildContext context) => PasswordBloc(_formRepository),
-          ),
-          BlocProvider<FormBloc>(
-            create: (BuildContext context) => FormBloc(),
-          ),
-        ],
+      home: BlocProvider(
+        create: (context) => AuthFormBloc(),
         child: LoginPage(),
       ),
     );
